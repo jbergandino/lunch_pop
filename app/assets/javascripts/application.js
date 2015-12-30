814df8b41476b06ea7aaa497288a04e14bd003d4
John Bergandino <jbergandino@gmail.com>
2015-12-29 23:29:53 -0500

    added better looking forms to custom pages front end and added fontawesome

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,89 @@
 //= require websocket_rails/main
 //= require_tree .
 
+$(document).ready(function() {
 
+      // Create New Vendor Page
+      $(".aaa").on("focusin", function(){
+         $("i.icon-a").toggleClass("fa-spin");
+      });
+      $(".aaa").on("focusout", function(){
+         $("i.icon-a").toggleClass("fa-spin");
+      });
+      $(".bbb").on("focusin", function(){
+         $("i.icon-b").toggleClass("fa-spin");
+      });
+      $(".bbb").on("focusout", function(){
+         $("i.icon-b").toggleClass("fa-spin");
+      });
+      $(".ccc").on("focusin", function(){
+         $("i.icon-c").toggleClass("fa-spin");
+      });
+      $(".ccc").on("focusout", function(){
+         $("i.icon-c").toggleClass("fa-spin");
+      });
+      $(".ddd").on("focusin", function(){
+         $("i.icon-d").toggleClass("fa-spin");
+      });
+      $(".ddd").on("focusout", function(){
+         $("i.icon-d").toggleClass("fa-spin");
+      });
+      $(".eee").on("focusin", function(){
+         $("i.icon-e").toggleClass("fa-spin");
+      });
+      $(".eee").on("focusout", function(){
+         $("i.icon-e").toggleClass("fa-spin");
+      });
+      $(".fff").on("focusin", function(){
+         $("i.icon-f").toggleClass("fa-spin");
+      });
+      $(".fff").on("focusout", function(){
+         $("i.icon-f").toggleClass("fa-spin");
+      });
+      $(".ggg").on("focusin", function(){
+         $("i.icon-g").toggleClass("fa-spin");
+      });
+      $(".ggg").on("focusout", function(){
+         $("i.icon-g").toggleClass("fa-spin");
+      });
+      $(".hhh").on("focusin", function(){
+         $("i.icon-h").toggleClass("fa-spin");
+      });
+      $(".hhh").on("focusout", function(){
+         $("i.icon-h").toggleClass("fa-spin");
+      });
+      $(".iii").on("focusin", function(){
+         $("i.icon-i").toggleClass("fa-spin");
+      });
+      $(".iii").on("focusout", function(){
+         $("i.icon-i").toggleClass("fa-spin");
+      });
+      $(".jjj").on("focusin", function(){
+         $("i.icon-j").toggleClass("fa-spin");
+      });
+      $(".jjj").on("focusout", function(){
+         $("i.icon-j").toggleClass("fa-spin");
+      });
+
+      // Create New Product Page
+      $(".product_a").on("focusin", function(){
+         $("i.icon-product-a").toggleClass("fa-spin");
+      });
+      $(".product_a").on("focusout", function(){
+         $("i.icon-product-a").toggleClass("fa-spin");
+      });
+      $(".product_b").on("focusin", function(){
+         $("i.icon-product-b").toggleClass("fa-spin");
+      });
+      $(".product_b").on("focusout", function(){
+         $("i.icon-product-b").toggleClass("fa-spin");
+      });
+      $(".product_c").on("focusin", function(){
+         $("i.icon-product-c").toggleClass("fa-spin");
+      });
+      $(".product_c").on("focusout", function(){
+         $("i.icon-product-c").toggleClass("fa-spin");
+      });
+
+
+  });
